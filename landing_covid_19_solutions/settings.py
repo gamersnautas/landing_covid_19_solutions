5d88eb5bb62dc745f57bb87a254f6ea721b8812b
gamersnautas <peraltahebert@gmail.com>
2020-07-28 20:59:08 -0400

    second commit

--- landing_covid_19_solutions/settings.py
+++ landing_covid_19_solutions/settings.py
@@ -28,7 +28,8 @@ DEBUG = True
 ALLOWED_HOSTS = [
     'http://pruebasysolucionescovit.com',
     'www.pruebasysolucionescovit.com',
-    'pruebasysolucionescovit.com'
+    'pruebasysolucionescovit.com',
+    '127.0.0.1'
 ]
 
 
